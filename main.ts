62fe91b64f0da1f6588bfcd8bbdd0235e31253ab
esdraslov <103470150+esdraslov@users.noreply.github.com>
2024-05-08 19:08:22 -0300

    descriptions

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+/**
+ * allow easy use of much arduino components, like buzzer and photoresistor
+ */
 //% icon="\uf287" color="#FE6423" weight=300 block="Multi Port"
 //% groups=["Buzzer", "Rotating system", "Light system", "Arduino", "other"]
 namespace MultiPort {
     //% group="Buzzer"
     export namespace buzzer {
+        /**
+         * turn on or off a connected buzzer
+         * 
+         * @param pin the pin whichs the buzzer/piezo is connected
+         * @param on the state of the buzzer/piezo
+         */
         //% block="set state of buzzer (piezo) in pin %pin to %on"
         //% group="Buzzer"
         export function TurnBuzzer(pin: DigitalPin, on: boolean) {
@@ -12,6 +21,11 @@ namespace MultiPort {
                 pins.digitalWritePin(pin, 0)
             }
         }
+        /**
+         * play note in a connected buzzer
+         * 
+         * @param note the note to be played
+         */
         //% block="buzzer in note %note"
         //% group="Buzzer"
         export function Hz(note: Note) {
@@ -20,6 +34,11 @@ namespace MultiPort {
     }
     //% group="Light system"
     export namespace photoresistor {
+        /**
+         * read the light detected by a photoresistor
+         * 
+         * @param pin the pin is the photoresistor
+         */
         //% block="read photoresistor at pin %pin"
         //% group="Light system"
         export function read(pin: AnalogPin) {
@@ -28,9 +47,17 @@ namespace MultiPort {
     }
     //% group="Arduino"
     export namespace DistanceSensor {
+        /**
+         * Check the distance of a sensor (sonar extension required)
+         * 
+         * @param trigger the trig
+         * @param echo the echo
+         * @param unit the unit
+         */
         //% block="check distance of a object using pins trig %trigger and echo %echo, result in %unit"
+        //% group="Arduino"
         export function checkDistance(trigger: DigitalPin, echo: DigitalPin, unit: PingUnit) {
-            return sonar.ping(DigitalPin.P0, DigitalPin.P0, PingUnit.Centimeters)
+            return sonar.ping(trigger, echo, unit)
         }
     }
 }
