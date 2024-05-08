7d5f2eb45d2b2c717b681b37914d476c6acba7f6
esdraslov <103470150+esdraslov@users.noreply.github.com>
2024-05-08 18:47:59 -0300

    groups fix

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 //% icon="\uf287" color="#FE6423" weight=300 block="Multi Port"
 //% groups=["Buzzer", "Rotating system", "Light system", "Arduino"]
 namespace MultiPort {
-    //% group="buzzer"
+    //% group="Buzzer"
     export namespace buzzer {
         //% block="set state of buzzer (piezo) in pin %pin to %on"
-        //% groups=["Buzzer"]
+        //% group="Buzzer"
         export function TurnBuzzer(pin: DigitalPin, on: boolean) {
             if (on) {
                 pins.digitalWritePin(pin, 1)
@@ -18,10 +18,10 @@ namespace MultiPort {
             music.tonePlayable(Note.C, music.beat(BeatFraction.Whole))
         }
     }
-    //% groups=["Light system", "Arduino"]
+    //% group="Light system"
     export namespace photoresistor {
         //% block="read photoresistor at pin %pin"
-        //% groups=["Light system", "Arduino"]
+        //% group="Light system"
         export function read(pin: AnalogPin) {
             return pins.analogReadPin(pin)
         }
