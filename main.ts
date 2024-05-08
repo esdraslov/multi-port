bdc84b47e8eaeadf05adfef6379b84cdf668230f
esdraslov <103470150+esdraslov@users.noreply.github.com>
2024-05-08 18:14:57 -0300

    play note in buzzer

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 namespace MultiPort {
     export namespace buzzer {
-        //% block="set buzzer frequency at pin %pin to %Hz"
-        export function SetFrequency(Hz: number, pin: DigitalPin) {
-            pins.digitalWritePin(pin, Hz)
+        //% block="set state of buzzer (piezo) in pin %pin to %on"
+        export function TurnBuzzer(on: boolean, pin: DigitalPin) {
+            if (on) {
+                pins.digitalWritePin(pin, 1)
+            } else {
+                pins.digitalWritePin(pin, 0)
+            }
+        }
+        //% block="buzzer in note %note"
+        export function Hz(note: Note) {
+            music.tonePlayable(Note.C, music.beat(BeatFraction.Whole))
         }
     }
 }
